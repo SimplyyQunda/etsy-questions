72e9c8cdc6b27d89e9ba9535ec09d22e6bbdbd78
DaQunda Magwood <dmagwood13@gmail.com>
2016-10-10 19:15:19 -0400

    finally have questions answers, with minor errors

---
+++ main.js
@@ -0,0 +1,140 @@
+//Question 1
+
+var answer1 = document.getElementById("answer1")
+
+function getPrice (items){
+	var prices = [];
+
+	for (var count = 0; count < items.length; count++){
+
+	var newPrice =(items[count].price);
+	prices.push(newPrice);
+	};
+
+	// console.log(prices)
+	return prices;
+};
+
+function sum(prices){
+	var addprices = 0;
+	for (var count =0; count< prices.length; count++){
+		addprices = (addprices + prices[count])
+
+	}
+		return addprices;
+
+}
+var allPrices = getPrice(items)
+var result = sum(allPrices)
+// console.log(result/items.length)
+// console.log(getPrice(items))
+answer1.innerHTML = result/items.length;
+
+// getPrice(items);
+
+
+//Question 2
+
+var getItemsPrice = function (items) {
+	var itemPrices = [];
+	for (var count = 0; count < items.length; count++) {
+		var exactPrice = items[count].price;
+		// console.log(Exactprice)
+		if (exactPrice > 14 && exactPrice < 18) {
+			itemPrices.push(items[count].title);
+		};
+
+	};
+	return itemPrices;
+	// console.log(itemPrices);
+};
+
+var answer2 = document.getElementById("answer2");
+var midPriceItems = getItemsPrice(items);
+
+answer2.innerHTML += getItemsPrice (items);
+
+
+//Question 3
+var answer1 = document.getElementById("answer3")
+
+
+
+var gbpcode = function(items){
+	var currency = [];
+	for(var count = 0; count < items.length; count++){
+		var currencytype = items[count].currency_code;
+		if(currencytype === "GBP"){
+			var newItem = items[count].title + ' costs ' + items[count].price;
+			currency.push(newItem);
+		};	
+		
+	};
+		return currency;
+};
+answer3.innerHTML += gbpcode (items);
+
+//Question 4
+var answer1 = document.getElementById("answer4")
+
+
+
+var madeofWood = function(items){
+	var listofWood = [];
+	for(var count = 0; count < items.length; count++){
+		var listtype = items[count].materials;
+		if(listtype === "wood"){
+			
+			listofWood.push(items[count].title);
+		};
+	};
+	return listofWood;
+	console.log(listofWood)
+};
+
+answer2.innerHTML += madeofWood (items);
+
+//Question 5
+
+var eight = items.filter(function(objects){
+	return objects.materials.length > 8;
+});
+
+ // for( count = 0; count < eight.length; count++){
+	// answer5.innerHTML +=  "<p>"+ eight[count].title +"</p>";
+
+
+	//Question 6
+
+	var sellers = items.filter(function(objects){
+		return objects.who_made === "i_did";
+	})
+// var answer6 = document.getElementById("answer6");
+// answer5.innerHTML = sellers.length;
+
+
+var madeofWood = function(items){
+	var listofWood = [];
+	for(var count = 0; count < items.length; count++){
+		var listtype = items[count].materials;
+		if(listtype === "wood"){
+			
+			listofWood.push(items[count].title);
+		};
+	};
+	return listofWood;
+	console.log(listofWood)
+};
+
+
+	
+
+
+
+
+
+
+
+
+
+
